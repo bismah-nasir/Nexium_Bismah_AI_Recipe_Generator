f45b8c49489ba6ecf397bb43a64e529368812318
Bismah <bismahnasir000@gmail.com>
2025-07-31 22:00:39 +0500

    Added Screenshot

--- internship/grand-project/src/middleware.ts
+++ internship/grand-project/src/middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    console.log('Running middleware....');
+    console.log("Running middleware....");
     const res = NextResponse.next();
     // console.log('REs:', res);
     const supabase = createMiddlewareClient({ req, res });
@@ -17,9 +17,11 @@ export async function middleware(req: NextRequest) {
 
     // Debugging: Log session status in middleware
     const { data } = await supabase.auth.getSession(); // Re-fetch for logging
-    const {session} = data;
-    console.log('Maryam session:', session);
-    console.log('Middleware: Session data:', session ? session.user.email : 'No session');
+    const { session } = data;
+    console.log(
+        "Middleware: Session data:",
+        session ? session.user.email : "No session"
+    );
 
     return res;
 }
