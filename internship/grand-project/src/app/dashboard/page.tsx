3b984511b3fc52d42952ecb01997af549be16fd9
Bismah <bismahnasir000@gmail.com>
2025-07-31 22:36:12 +0500

    (fix) localStorage server pre-rendering issue

--- internship/grand-project/src/app/dashboard/page.tsx
+++ internship/grand-project/src/app/dashboard/page.tsx
@@ -1,22 +1,18 @@
 "use client";
 
+import { useEffect } from "react";
 import DashboardPage from "@/components/DashboardPage";
 import { useRouter } from "next/navigation";
 export default function Page() {
-    // MODIFIED LINE: Changed from `cookies: () => cookies()` back to `cookies` directly.
-    // This is the standard and most robust way for createServerComponentClient in App Router.
-    // const supabase = createServerComponentClient({ cookies });
-
-    // const {
-    //     data: { session },
-    // } = await supabase.auth.getSession();
-    const { session } = JSON.parse(localStorage.getItem("user-info")!);
     const router = useRouter();
-    // console.log('Login session:', session);
 
-    if (!session) {
-        router.replace("/signin");
-    }
+    useEffect(() => {
+        const { session } = JSON.parse(localStorage.getItem("user-info")!);
+
+        if (!session) {
+            router.replace("/signin");
+        }
+    }, []);
 
     return (
         <div>
